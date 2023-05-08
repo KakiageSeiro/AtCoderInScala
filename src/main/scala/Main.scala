1b3ee01d449dd6e4257ca753df0102885ce88564
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2023-05-09 00:07:16 +0900

    提出するときは標準出力にださなければいけなかった

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,15 @@
-import 過去問精選10問.ABC086A_Product
+
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(ABC086A_Product.run())
+    // println(ABC086A_Product.run())
+    println(run())
+  }
+
+  def run(): String = {
+    val sc = new java.util.Scanner(System.in)
+    val a, b = sc.nextInt()
+    val c = a * b
+    if (c % 2 == 0) "Even" else "Odd"
   }
 }
